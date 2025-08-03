5f97ce112a7034e1b2f632db336e142e6a1ad19f
Sai Pavan <saipavansurya123.in@gmail.com>
2025-08-03 20:53:55 +0530

    Database Modify

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ const app = express();
 const port = 8000;
 
 const db = new pg.Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
 
 db.connect();
 
